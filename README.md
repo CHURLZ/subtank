b7a6728af77315c6e472b692b7cf3314a78c7cd9
CHURLZ <kalle.berglund@gmaik.com>
2017-11-07 23:46:39 +0100

    updated script for py3

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # subtank
-python script to download a subtitle for provided video file from thesubdb.com
+python3 script to download a subtitle for provided video file (or files in folder) from thesubdb.com
+
 
 #### usage
 run script with:
 ```python
-  python hash.py fileName
+  python hash.py (fileName)
   ```
-  
-  
--- hash.py
+++ hash.py
@@ -1,5 +1,9 @@
-import sys,os,hashlib
-import urllib2
+import sys
+import os
+import hashlib
+import requests
+
+media_extension = ['mp4', 'mkv', 'avi', 'mpg', 'mov', 'm4v']
 
 def get_hash(name):
     readsize = 64 * 1024
@@ -10,30 +14,41 @@ def get_hash(name):
         data += f.read(readsize)
     return hashlib.md5(data).hexdigest()
 
-def get_request(hash):
-    url = 'http://api.thesubdb.com/?action=download&hash='+hash+'&language=en'
-    request = urllib2.Request(url)
-    request.add_header('User-agent', 'SubDB/1.0 (SubTank/0.1; http://github.com/CHURLZ/subtank)')
-    return request
 
-def get_subname(file):
-    fileName = file
-    filelength = len(file)
-    if(file[filelength-4]) == '.':
-        fileName = file[:-4]
-    elif(file[filelength-5]) == '.':
-        fileName = file[:-5]
-    return fileName + '.srt'
+def make_request(hash):
+    url = 'http://api.thesubdb.com/?action=download&hash={}&language=en'.format(hash)
+    headers = {'User-agent': 'SubDB/1.0 (SubTank/0.1; http://github.com/CHURLZ/subtank)'}
+    response = requests.get(url, headers=headers)
+    return response if response.status_code == 200 else None
 
-file = sys.argv[1]
-hash = get_hash(file)
 
-contents = urllib2.urlopen(get_request(hash))
-if contents.getcode() == 200:
-    print("printing sub to file:" + get_subname(file))
-    output = open(get_subname(file), 'w')
-    output.write(contents.read())
-    output.close()
+def download_sub(file_name):
+    response = make_request(get_hash(file_name))
+    if not response:
+        print('### ERROR downloading subs for\n\t{}'.format(file_name))
+        return False
+    write_sub_to_file(file_name, response.content)
+    return True
+
+
+def write_sub_to_file(file_name, byte_data):
+    sub_file_name = '{}.srt'.format('.'.join(file_name.split('.')[:-1]))
+    print('printing sub to file: {}'.format(sub_file_name))
+    with open(sub_file_name, 'wb') as f:
+        f.write(byte_data)
+
+
+def rq(f):
+    print('### ERROR file not found!\n\t{}'.format(f))
+    sys.exit()
+
+downloads = 0
+if len(sys.argv) < 2:
+    for f in os.listdir():
+        if f.split('.')[-1] in media_extension:
+            downloads += 1 if download_sub(f) else 0
 else:
-    print("### ERROR ###")
-    print(contents.read())
+    f = sys.argv[1]
+    rq(f) if f not in os.listdir() else download_sub(f)
+
+print('{} subs downloaded in folder {}.'.format(downloads, os.getcwd()))
